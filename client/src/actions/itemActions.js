43524b2034592aa8f4cc46068224f35a0a7ae0c8
Khorf Zakaria <gz_khorf@esi.dz>
2021-02-04 20:54:49 +0100

    Shopping list finished

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -3,21 +3,20 @@ import axios from 'axios'
 export const getItems = () => dispatch => {
     dispatch(setItemsLoading());
     axios.get('/api/items').then(res =>
-        dispatch({ 
-            type: GET_ITEMS, 
-            payload: res.data 
+        dispatch({
+            type: GET_ITEMS,
+            payload: res.data
         })
     )
 }
-export const deleteItems = (id) => {
-    return {
+export const deleteItems = (id) => dispatch => {
+    axios.delete(`/api/items/${id}`).then(res => dispatch({
         type: DELETE_ITEMS,
         payload: id
-
-    }
+    }))
 }
 export const addItem = (item) => dispatch => {
-    axios.post('/api/items',item).then(res=>{dispatch({type: ADD_ITEMS,payload: res.data})})
+    axios.post('/api/items', item).then(res => { dispatch({ type: ADD_ITEMS, payload: res.data }) })
 
 
     return {
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -25,14 +25,14 @@ render () {
        
           <ListGroup>
               <TransitionGroup className="shopping-list">
-                  {items.map(({id , name})=>(
-                    <CSSTransition key={id} timeout={500} classNames="fade">
+                  {items.map(({_id , name})=>(
+                    <CSSTransition key={_id} timeout={500} classNames="fade">
                         <ListGroupItem>
                             <Button
                             className="remove-btn"
                             color="danger"
                             size="sm"
-                            onClick={this.onDeleteClick.bind(this,id)}>&times;</Button>
+                            onClick={this.onDeleteClick.bind(this,_id)}>&times;</Button>
                             {name}
                         </ListGroupItem>
                     </CSSTransition>
