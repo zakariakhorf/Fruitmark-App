33e8f0de0e72ef5b54f214abe455ab8802bd74ad
Khorf Zakaria <gz_khorf@esi.dz>
2021-02-05 01:13:57 +0100

    Finished the shoppingList

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
 const items = require('./routes/api/items')
-
+const path = require('path')
 
 
 
 
 app.use(bodyParser.json())
-
+// serve static ssets if in production mode
+if(process.env.NODE_ENV === 'production') {
+    //set static folder 
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 
 //DB Config
