5496536beb2335d3145010dd035904f9cfc8bed9
Khorf Zakaria <gz_khorf@esi.dz>
2021-02-07 14:16:32 +0100

    Fin

--- server.js
+++ server.js
@@ -26,13 +26,19 @@ mongoose.connect(db)
 app.use('/api/items',items)
 
 
-if(process.env.NODE_ENV === 'production') {
-    //set static folder 
-    app.use(express.static('client/build'))
-    app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+
+
+
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
 
 
 
